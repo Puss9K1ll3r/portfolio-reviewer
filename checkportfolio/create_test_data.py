98351339050f73d311d090258c2ccd8d8c481caa
Puss9K1ll3r <bogdan2006raf@gmail.com>
2025-05-13 16:34:34 +0500

    Add docker

--- checkportfolio/create_test_data.py
+++ checkportfolio/create_test_data.py
@@ -10,7 +10,7 @@ from django.contrib.auth.hashers import make_password
 def create_test_data():
     # 1. Создаем тестового пользователя (учителя)
     teacher_user = TeacherData.objects.create(
-        username='test_teacher',
+        username='teacher_2',
         password=make_password('teacher123'),
         is_staff=True,
         is_active=True
@@ -19,42 +19,42 @@ def create_test_data():
     # 2. Создаем запись в Teacher
     teacher = Teacher.objects.create(
         user=teacher_user,
-        name="Иванова Мария Сергеевна"
+        name="Иванов Иван Иванович"
     )
     
-    # 3. Создаем несколько тестовых предметов
-    subjects = [
-        {
-            'subject_id': 1,
-            'title': 'Математика',
-            'abbr': 'Мат',
-            'files_count': 10,
-            'review_params': 'Точность вычислений, логичность рассуждений',
-            'changer': teacher
-        },
-        {
-            'subject_id': 2,
-            'title': 'Физика',
-            'abbr': 'Физ',
-            'files_count': 8,
-            'review_params': 'Правильность формул, единицы измерения',
-            'changer': teacher
-        },
-        {
-            'subject_id': 3,
-            'title': 'Информатика',
-            'abbr': 'Инф',
-            'files_count': 15,
-            'review_params': 'Корректность кода, комментарии',
-            'changer': teacher
-        }
-    ]
+    # # 3. Создаем несколько тестовых предметов
+    # subjects = [
+    #     {
+    #         'subject_id': 1,
+    #         'title': 'Математика',
+    #         'abbr': 'Мат',
+    #         'files_count': 10,
+    #         'review_params': 'Точность вычислений, логичность рассуждений',
+    #         'changer': teacher
+    #     },
+    #     {
+    #         'subject_id': 2,
+    #         'title': 'Физика',
+    #         'abbr': 'Физ',
+    #         'files_count': 8,
+    #         'review_params': 'Правильность формул, единицы измерения',
+    #         'changer': teacher
+    #     },
+    #     {
+    #         'subject_id': 3,
+    #         'title': 'Информатика',
+    #         'abbr': 'Инф',
+    #         'files_count': 15,
+    #         'review_params': 'Корректность кода, комментарии',
+    #         'changer': teacher
+    #     }
+    # ]
     
-    for subj in subjects:
-        Subject.objects.create(**subj)
+    # for subj in subjects:
+    #     Subject.objects.create(**subj)
     
     print("Тестовые данные созданы успешно!")
-    print(f"Логин: test_teacher, Пароль: teacher123")
+    # print(f"Логин: test_teacher, Пароль: teacher123")
 
 if __name__ == '__main__':
     create_test_data()
